fe7841e9bdb2abf6f4e0a751c15d1806166b91c2
Zaryab Akhtar <zaryabakhtar9@gmail.com>
2022-06-15 20:10:06 +0400

    Initial Commit

---
+++ app/Http/Controllers/WhatsAppController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WhatsAppController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index(Request $request){
+        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        fwrite($myfile, $request);
+        fclose($myfile);
+    }
+}
